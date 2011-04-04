602168f8933239529a2be8aba8d0a327dfc5c4ba
Ian Clelland <clelland@gmail.com>
2011-04-04 13:28:08 -0700

    Add flags for proper escaping

--- restless.js
+++ restless.js
@@ -17,6 +17,23 @@ restless = (function() {
 
     var version = "0.1";
 
+    function encodeReserved(string) {
+        // TODO: This, faster
+        var output = "";
+        for (var index=0; index < string.length; index++) {
+            var chr = string.charCodeAt(index);
+            if (((chr >= 0x41) && (chr <= 0x5a)) ||
+                ((chr >= 0x61) && (chr <= 0x7a)) ||
+                ((chr >= 0x30) && (chr <= 0x39)) ||
+                (chr === 0x2d || chr === 0x3e || chr === 0x5f || chr === 0x73)) {
+                output += String.fromCharCode(chr);
+            } else {
+                output += '%'+chr.toString(16).toUpperCase();
+            }
+        }
+        return output;
+    }
+
     function _resolve(parts, context) {
         var output = [];
         for (var index=0; index < parts.length; index++) {
@@ -29,9 +46,9 @@ restless = (function() {
                     var r = part.replacements[i];
                     var v = r['var'] && context[r['var']] || r['default'];
                     if (v instanceof Array) {
-                        replacements.push(v.join(','));
+                        replacements.push(\u1403.map(v,(part.flags==='+'?encodeURI:encodeReserved)).join(','));
                     } else {
-                        replacements.push(v);
+                        replacements.push((part.flags==='+'?encodeURI:encodeReserved)(v));
                     }
                 }
                 output.push(replacements.join(','));
@@ -41,19 +58,19 @@ restless = (function() {
     }
 
     function tokenize(string) {
-        var expression = /\{((\w+)(=(\w+))?(,(\w+)(=(\w+))?)*)\}/;
+        var expression = /\{(\+)?((\w+)(=(\w+))?(,(\w+)(=(\w+))?)*)\}/;
         var parts = [];
         while (string) {
             var index = string.search(expression);
             if (index === 0) {
                 var match = expression.exec(string);
-                var vars = match[1].split(',');
+                var flags = match[1], vars = match[2].split(',');
                 var replacements = [];
                 for (var i = 0; i < vars.length; i++) {
                     var varparts = vars[i].split('=');
                     replacements.push({ 'var': varparts[0], 'default': varparts[1] });
                 }
-                parts.push({ replacements: replacements });
+                parts.push({ replacements: replacements, flags: flags });
                 string = string.slice(match[0].length);
             } else {
                 var part = (index === -1) ? string : string.slice(0, index);
